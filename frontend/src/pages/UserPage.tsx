fd2b863f0c57df4f0023633961b3d4a453d30de1
mathysard <148949076+mathysard@users.noreply.github.com>
2026-08-22 19:07:06 +0200

    Comment out profile modification button
    Comment out the profile modification button for 'me' user.

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -112,7 +112,7 @@ export default function UserPage() {
                         </span>
                     </div> */}
 
-                    {userId === "me" && (<button className="px-4 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-800 hover:cursor-pointer active:bg-blue-900">Modifier le profil</button>)}
+                    {/* {userId === "me" && (<button className="px-4 py-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-800 hover:cursor-pointer active:bg-blue-900">Modifier le profil</button>)} */}
                 </div>
 
                 <div className="space-y-4">
